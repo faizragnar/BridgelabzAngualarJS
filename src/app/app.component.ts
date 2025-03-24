de2be163487c9f926336284fd46025950baf0ae3
unknown <faiyazamail@gmail.com>
2025-03-24 17:15:31 +0530

    [Faiyaj ahmed] Add . Event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   message: string = "Hello from BridgeLabz";
-  imgUrl = "https://media.licdn.com/dms/image/v2/C510BAQEYyeT3P0H_mw/company-logo_200_200/company-logo_200_200/0/1630611149363/bridgelabz_com_logo?e=2147483647&v=beta&t=TFgRiaA55f57NqQFnUbnsKu3mQo7c-LOfD8_iNSgtNM"
-}
+  imgUrl: string = "https://media.licdn.com/dms/image/v2/C510BAQEYyeT3P0H_mw/company-logo_200_200/company-logo_200_200/0/1630611149363/bridgelabz_com_logo?e=2147483647&v=beta&t=TFgRiaA55f57NqQFnUbnsKu3mQo7c-LOfD8_iNSgtNM";
+  bridgeLabzUrl: string = "https://www.bridgelabz.com";
 
+  launchBridgeLabz() {
+    window.open(this.bridgeLabzUrl, "_blank"); // Opens URL in a new tab
+  }
+}
