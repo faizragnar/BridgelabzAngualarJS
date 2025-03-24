17eac2791648edb22b8945d652cdba63d500d9ea
unknown <faiyazamail@gmail.com>
2025-03-24 17:13:49 +0530

    [Faiyaj ahmed] Add . image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  message: string = 'Hello from BridgeLabz';
+  message: string = "Hello from BridgeLabz";
+  imgUrl = "https://media.licdn.com/dms/image/v2/C510BAQEYyeT3P0H_mw/company-logo_200_200/company-logo_200_200/0/1630611149363/bridgelabz_com_logo?e=2147483647&v=beta&t=TFgRiaA55f57NqQFnUbnsKu3mQo7c-LOfD8_iNSgtNM"
 }
+
